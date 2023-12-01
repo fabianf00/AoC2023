527a0055d3a8f6a19401a859e4e5c7844f6cfaa0
fabianf00 <fabian.frauenberger@me.com>
2023-12-01 21:54:18 +0100

    feat: day01 part 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,15 +1,15 @@
 import re
 
 DIGITS = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 
 
@@ -22,27 +22,53 @@ def split_lines(input) -> list:
     return input.split("\n")
 
 
-def check_digit(char) -> int:
-    if char.isdigit():
-        return int(char)
-
-
 def part1(lines):
-    calibration_values = []
+    total = 0
     for line in lines:
         digits = re.findall(r"\d", line)
         first_digit = digits[0]
         second_digit = digits[-1]
-        calibration_values.append(int(first_digit + second_digit))
-    return sum(calibration_values)
+        total += int(first_digit + second_digit)
+    return total
+
+
+def find_first_digit(line) -> str:
+    if line[0].isdigit():
+        return line[0]
+
+    # See if string matches any of the keys in DIGITS
+    res = next(filter(line.startswith, DIGITS.keys()), None)
+
+    return DIGITS.get(res, None)
+
+
+def part2(lines):
+    total = 0
+    for line in lines:
+        first_digit = None
+        second_digit = None
+        for i in range(len(line)):
+            first_digit = find_first_digit(line[i:])
+            if first_digit:
+                break
+        for i in range(len(line)):
+            second_digit = find_first_digit(line[-1 * i - 1 :])
+            if second_digit:
+                break
+        total += int(first_digit + second_digit)
+    return total
+
+
+input = read_file("input.txt")
+lines = split_lines(input)
 
 
 ## Part 1
-input = read_file("part_1.txt")
-lines = split_lines(input)
-
-result = part1(lines)
-
-print(result)
+print("--- Part 1 ---")
+result1 = part1(lines)
+print(result1)
 
 ## Part 2
+print("--- Part 2 ---")
+result2 = part2(lines)
+print(result2)
